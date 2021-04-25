fb6ef65c10ae41e7c89ba2d8da3104b11736fe2a
OdehYazan <eng.yazanodeh@gmail.com>
2021-04-25 16:20:47 +0300

    class1

---
+++ js/script.js
@@ -0,0 +1,21 @@
+'use strict'
+let uName = prompt('What is your name ?');
+alert ('Welcome '+uName);
+console.log('User name = '+uName);
+
+let newAnime = prompt('which anime your watching these days ?');
+alert (newAnime +' WOW good one !');
+console.log('watching now = '+newAnime);
+
+let favAnime = prompt('What is your favorite anime ?');
+alert (favAnime+' Me too ');
+console.log('fav anime is '+favAnime)
+
+let seenAnime = prompt('Do you like Attack on Titans ? yes or no')
+if (seenAnime == 'yes'){
+    alert ('Hello '+uName+' I am watching '+newAnime+' too it is amazing and '+favAnime+' is my favorite anime just like you, '+seenAnime+'for Attack on Titans is a big like ' );
+    console.log('Attck on titan seen = '+seenAnime);
+}else{
+    alert ('Hello '+uName+' I am watching '+newAnime+' too it is amazing and '+favAnime+' is my favorite anime just like you, '+seenAnime+'for Attack on Titans you should change your mind ' );
+    console.log('Attck on titan seen = '+seenAnime);
+}
